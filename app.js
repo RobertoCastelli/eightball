721d850e3fa5eccc547c3f42d4d1290f5b70701f
feticcio <feticcio@pop-os.localdomain>
2020-02-22 19:40:55 +0100

    added fontawsome

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-let question = document.getElementById('question')
-let response = document.getElementById('response')
+let question = document.getElementById('input-question')
+let displayQuestion = document.getElementById('display-question')
 let btnSubmit = document.getElementById('btnSubmit')
+let image = document.getElementById('display-ball')
+
+image.src = 'img/ballfront.png'
 
 btnSubmit.addEventListener('click', () => {
     let quest = question.value
     question.value = ''
+    displayQuestion.innerText = quest
     getData(quest)
 })
 
 async function getData(request) {
-    try{
+    try {
         let uri = encodeURIComponent(request);
         let resp = await fetch("https://8ball.delegator.com/magic/JSON/" + uri)
         let json = await resp.json()
-        response.innerText = json.magic.answer
         console.log(json.magic.answer)
     } catch (err) {
         console.log(err, "Some error occurred")
     }
 }
-
